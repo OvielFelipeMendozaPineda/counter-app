67b564e63c2bf60d7aea74a9ac9b641f0539664f
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-07-31 23:03:58 -0500

    feat: Instalado tailwind

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import CounterApp from "./CounterApp";
 
 
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={10} />
+        {/* <CounterApp value={10} /> */}
+        <h1 className="text-3xl font-bold ">
+      Hello world!
+    </h1>
     </React.StrictMode> 
 )
